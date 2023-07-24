54029b3c5b479736cc47aaf1a4f2f1d8eb3089c5
Ken H. Dizon <90869022+khdizon@users.noreply.github.com>
2023-07-24 16:44:18 -0600

    Add files via upload

---
+++ Markdown_Functions.py
@@ -0,0 +1,60 @@
+### MARKDOWN ###
+
+### Table ### 
+X1  | X2 | X3 | X4 |
+:--:|:--:|:---|:--:|
+N1 | N2 | N3 | N4 |
+
+### HEADINGS ###
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+
+**This is bold text**  
+__This is bold text__
+
+*This is italic text*  
+_This is italic text_
+
+***Bold and Italic***  
+___Bold and Italic___
+
+~~Scratch this~~
+
+> This is a blockquote
+>> Nested Block
+>>> Nested Block2
+
+### LINES ###
+---
+___
+***
+
+### LISTING ###
+1. Cheese
+2. Carrot
+3. Coconut
+or
+- Cheese
+- Carrot
+- Coconut
+or
+- [x] Some task
+- [ ] Some more task
+
+### GRAPHICS/IMAGE ###
+![](https://www.python.org/static/community_logos/python-logo-master-v3-TM.png
+   "hover title")
+
+### Hyperlink ##
+[click me](https://en.wikipedia.org)
+
+### blockcode ###
+```python
+s = "Python syntax highlighting"
+print s
+```
